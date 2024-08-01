fc25523641ea9e4d650f1f863169213563debbaf
internashionalist <taylor.thames@atlasschool.com>
2024-08-01 15:09:18 -0500

    betty

--- swap.c
+++ swap.c
@@ -10,19 +10,19 @@
 
 void swap(stack_t **stack, unsigned int line_number)
 {
-       stack_t *first_node; /* first node to swap */
-       stack_t *second_node; /* second node to swap */
-       int temp; /* to store value of first node */
+	stack_t *first_node; /* first node to swap */
+	stack_t *second_node; /* second node to swap */
+	int temp; /* to store value of first node */
 
-       if (!stack || !*stack || !(*stack)->next)
-       {
-              fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-              exit(EXIT_FAILURE);
-       }
-       first_node = *stack; /* store head in first_node */
-       second_node = (*stack)->next; /* store next node in second_node */
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	first_node = *stack; /* store head in first_node */
+	second_node = (*stack)->next; /* store next node in second_node */
 
-       temp = first_node->n; /* store value of first node */
-       first_node->n = second_node->n; /* set value of first node to second */
-       second_node->n = temp; /* set value of second node to temp */
+	temp = first_node->n; /* store value of first node */
+	first_node->n = second_node->n; /* set value of first node to second */
+	second_node->n = temp; /* set value of second node to temp */
 }
