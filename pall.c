2c6e7851158318a3fab44697d77d32c855053873
internashionalist <taylor.thames@atlasschool.com>
2024-07-31 20:26:05 -0500

    pall.0 - pall draft

--- pall.c
+++ pall.c
@@ -10,13 +10,13 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-       stack_t *current_node = *stack; /* current node to print */
+	stack_t *current_node = *stack; /* current node to print */
 
-       (void)line_number; /* unused */
+	(void)line_number; /* unused */
 
-       while (current_node) /* while there are still nodes to print */
-       {
-              printf("%d\n", current_node->n); /* print value of current node */
-              current_node = current_node->next; /* move to next node */
-       }
+	while (current_node) /* while there are still nodes to print */
+	{
+		printf("%d\n", current_node->n); /* print value of current node */
+		current_node = current_node->next; /* move to next node */
+	}
 }
