93d842d1aec1d10b1a1c4c42cce585bb92fc98c9
internashionalist <taylor.thames@atlasschool.com>
2024-07-31 21:15:10 -0500

    main.2 - read, exec opcode

--- main.c
+++ main.c
@@ -30,3 +30,12 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
+	while ((read = getline(&line, &len, file)) != -1) /* read line from file */
+	{
+		line_number++; /* increment line number */
+		opcode = strtok(line, " \t\n"); /* get opcode */
+		if (!opcode || opcode[0] == '#') /* if no opcode or comment */
+			continue; /* skip that line */
+		execute_instruction(opcode, &stack, line_number); /* execute opcode */
+	}
