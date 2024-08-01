d1accac89f4d2b300859d4fef1d8b8bf52d03ad3
internashionalist <taylor.thames@atlasschool.com>
2024-07-31 20:25:49 -0500

    instructions.0 - made a digit-checker function, started on execution function

--- monty.h
+++ monty.h
@@ -44,5 +44,7 @@ typedef struct instruction_s
 
 void push(stack_t **stack, unsigned int line_number, int n);
 void pall(stack_t **stack, unsigned int line_number);
+int is_digit(char *str);
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number);
 
 #endif
