e3bcf864ccf70295c150cef36e3761b0c534af45
internashionalist <taylor.thames@atlasschool.com>
2024-07-31 21:19:14 -0500

    free.0 - helper function to keep main under 40 lines

---
+++ free.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * free_stack - frees stack
+ * @stack: pointer to head of stack
+ *
+ * Return: void
+ */
+
+void free_stack(stack_t *stack)
+{
+       stack_t *current_node; /* current node to free */
+
+       while (stack) /* while there are nodes to free */
+       {
+               current_node = stack; /* start at head, store in current_node */
+               stack = stack->next; /* move head to next node */
+               free(current_node); /* free current node, repeat */
+       }
+}
