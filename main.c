bfd999dba792d8de1222d778c0f24735c1ba139b
internashionalist <taylor.thames@atlasschool.com>
2024-07-31 21:06:11 -0500

    main.1 - error cases

--- main.c
+++ main.c
@@ -17,7 +17,16 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL; /* head of stack */
 	unsigned int line_number = 0; /* current line number */
 	char *opcode; /* operation code to execute */
-	
-	
-	
-	
+
+	if (argc != 2) /* if number of arguments isnt 2 */
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r"); /* open file for reading */
+	if (!file) /* if file cannot be opened */
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
